bde2511b532ff6d36b856886bc943acfcdc590c2
Tushar Agarwal <108838006+TusharAgarwal10@users.noreply.github.com>
2024-02-12 21:48:08 +0530

    Update PL1_Exp1_A.py

--- Experiment 1/PL1_Exp1_A.py
+++ Experiment 1/PL1_Exp1_A.py
@@ -19,6 +19,8 @@ intercept = approx[1]
 
 ftp = slope*mass+intercept
 
+r_err = 0.001
+
 plt.scatter(mass, atp, color = "red", label = "Data Points")
 plt.plot(mass, ftp,  color = "blue", label = "Fit")
 plt.ylabel("Time Period (seconds)")
@@ -26,5 +28,6 @@ plt.xlabel("Mass (g)")
 plt.ylim([0, 2])
 plt.xlim([20, 40])
 plt.text(22.5,1.5,fr'$y = {np.round(slope,2)}x + {np.round(intercept,2)}$')
+plt.errorbar(mass, atp, yerr = r_err, capsize=5, ecolor = "black", label = "error", linestyle = "None")
 plt.legend()
 plt.show() 
