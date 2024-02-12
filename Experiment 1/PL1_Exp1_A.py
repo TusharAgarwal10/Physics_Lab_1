65ab0cb776daf4ea22819f0134702c05a57946ad
Tushar Agarwal <108838006+TusharAgarwal10@users.noreply.github.com>
2024-02-12 17:41:20 +0530

    Add files via upload

---
+++ Experiment 1/PL1_Exp1_A.py
@@ -0,0 +1,30 @@
+# Part A
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# mass of the blob used in the simple pendulum
+mass = np.array([34, 27, 25])
+
+# time period (raw data collected) in seconds
+
+tp = np.array([[9.15, 9.25, 9.2], [9.25, 9.15, 9.25], [9.19, 9.31, 9.31]])/10.0
+
+atp = np.round(np.average(tp, axis = 0), 2)
+
+approx = np.polyfit(mass, atp, 1)
+
+slope = approx[0]
+intercept = approx[1]
+
+ftp = slope*mass+intercept
+
+plt.scatter(mass, atp, color = "red", label = "Data Points")
+plt.plot(mass, ftp,  color = "blue", label = "Fit")
+plt.ylabel("Time Period (seconds)")
+plt.xlabel("Mass (g)")
+plt.ylim([0, 2])
+plt.xlim([20, 40])
+plt.text(22.5,1.5,fr'$y = {np.round(slope,2)}x + {np.round(intercept,2)}$')
+plt.legend()
+plt.show() 
